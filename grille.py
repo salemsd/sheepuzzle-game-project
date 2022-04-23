4cb5b116c4190aad0425042fa5c419aad6fc8ab4
salem <salem.saoudi@outlook.com>
2022-04-23 17:07:07 +0200

    Mise à jour du chargement de la grille (vérification du formatage + commentaires)

--- grille.py
+++ grille.py
@@ -10,19 +10,28 @@ def charger(fichier):
     plateau = []
     moutons = []
     i = 0 # Indice de la ligne
+    rowLengths = [] # Liste pour sauvegarder la taille de chaque ligne de la grille et les comparer
 
     for line in fPlateau:
+        line = line.strip() # Supprime le caractère \n (nouvelle ligne) qui est à chaque fin de ligne
+        rowLengths.append(len(line)) # Sauvegarde la taille de la ligne actuelle
+        if len(rowLengths) > 1 and rowLengths[i] != rowLengths[i-1]: # Si la taille change d'une ligne à une autre,
+            return None, None                                        # le fichier n'est pas correctement formaté
+        
         row = []
         j = 0 # Indice de la colonne qui sera reinitialisé à 0 à chaque changement de ligne
+        
         for char in line:
-            if char != '\n':
-                if char == '_':
-                    row.append(None)
-                elif char == 'S':
-                    moutons.append((i, j))
-                else:
-                    row.append(char)
+            if char not in ['B', 'S', 'G', '_']: # Si l'un des caractères ne correspond pas
+                return None, None                # le fichier n'est pas correctement formaté
+            if char == '_':
+                row.append(None)
+            elif char == 'S':
+                moutons.append((i, j)) # Sauvegarde les coordonnées du mouton dans une liste à part pour les moutons
+            else:
+                row.append(char)
             j = j + 1
+        
         plateau.append(row)
         i = i + 1
     
